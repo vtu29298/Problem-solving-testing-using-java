7494afabbe7287313c8be5d7c833c9dea3937403
vtu29298 <nikhitha272006@gmail.com>
2026-09-12 09:15:50 +0530

    Add files via upload

---
+++ week 7/session2  task4.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class BrowserHistory {
+
+    ArrayList<String> history = new ArrayList<>();
+    int current = 0;
+
+    public BrowserHistory(String homepage) {
+        history.add(homepage);
+    }
+    
+    public void visit(String url) {
+        while (history.size() > current + 1) {
+            history.remove(history.size() - 1);
+        }
+
+        history.add(url);
+        current++;
+    }
+    
+    public String back(int steps) {
+        current = Math.max(0, current - steps);
+        return history.get(current);
+    }
+    
+    public String forward(int steps) {
+        current = Math.min(history.size() - 1, current + steps);
+        return history.get(current);
+    }
+}
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory obj = new BrowserHistory(homepage);
+ * obj.visit(url);
+ * String param_2 = obj.back(steps);
+ * String param_3 = obj.forward(steps);
+ */
